2c149ac36b979d17aa006c733534ba74d28efd79
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-12-20 22:14:44 +0100

    Some changes to script to parse and query a slow query

--- RDF-Query/bin/query.pl
+++ RDF-Query/bin/query.pl
@@ -10,23 +10,6 @@ use RDF::Query;
 use RDF::Query::Util;
 binmode( \*STDOUT, ':utf8' );
 
-unless (@ARGV) {
-	print STDERR <<"END";
-USAGE:
-       $0 -e 'SELECT * ...' data.rdf [ ... ]
-       $0 -E http://service.example/sparql -e 'SELECT * ...'
-       $0 -F path/to/service_description.ttl -e 'SELECT * ...'
-       $0 query.rq data.rdf
-
-Reads in a SPARQL query from query.rq (or specified with the -e flag), and
-RDF/XML data from the data.rdf files. The SPARQL query is executed against a
-triplestore containing data from the data files, and query results are printed
-to standard output.
-
-END
-	exit;
-}
-
 ################################################################################
 # Log::Log4perl::init( \q[
 # 	log4perl.category.rdf.query.util		= DEBUG, Screen
@@ -38,16 +21,34 @@ END
 ################################################################################
 
 # construct a query from the command line arguments
-my ($query, $model)	= &RDF::Query::Util::cli_make_query_and_model;
+my $query = RDF::Query->new('PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#> SELECT ?server (count(?server) AS ?count) WHERE { GRAPH ?g { ?site <urn:app:freshtime:hard> ?hard ;  <urn:app:hasrequest> ?request .   ?request <http://www.w3.org/2007/ont/http#hasResponse> ?response .  ?response <http://www.w3.org/2007/ont/httph#server> ?server .  FILTER (xsd:integer(?hard) > 0) }} GROUP BY ?server ORDER BY ?count');
+
+use RDF::Trine;
+
+my $store = RDF::Trine::Store::Memory->new_with_string('Memory;file://'.join(';file://', glob('/mnt/ssdstore/data/btc-processed/run6/crawl/*')));
+my $model = RDF::Trine::Model->new($store);
+
+
+
+warn "load complete";
 unless ($query and $model) {
 	die RDF::Query->error;
 }
 
+warn "planning";
+my ($plan, $context) = $query->prepare ( $model );
+
+warn $plan->explain('  ', 0);
+
 # execute the query against data contained in the model
-my $iter	= $query->execute( $model );
+my $iter	= $query->execute_plan ( $plan, $context );
 
 # print the results as a string to standard output
 print $iter->as_string;
+
+print "\n";
+
+#print $plan->explain('  ', 0);
 
 ### this will allow the results to be printed in a streaming fashion:
 ### or, if you want to iterate over each result row:
